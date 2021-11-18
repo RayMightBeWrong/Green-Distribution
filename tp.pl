2d06829dcdc218f071edf7591ef8009d3113ee78
RayMightBeWrong <ruizinho7braga@gmail.com>
2021-11-18 23:25:56 +0000

    +1 predicado e adicionar datas + diferença entre entrega e encomenda + preço

--- tp.pl
+++ tp.pl
@@ -1,6 +1,6 @@
-estafeta(n1).
-estafeta(n2).
-estafeta(n3).
+estafeta(e1).
+estafeta(e2).
+estafeta(e3).
 
 %meio_de_transporte(volume máximo em kg, v média em km/h)
 max_bicicleta(5, 10).
@@ -13,11 +13,13 @@ cliente(c2).
 cliente(c3).
 
 
-%encomenda
-%adicionar dia
-%prazo_de_entrega, preço
+%adicionar local, distância, hora em vez de data(?)
 
-g([entrega(1, n1, c1, bicicleta, 4), entrega(2, n2, c1, moto, 6), entrega(2, n1, c2, moto, 6)]).
+%encomenda(nr, cliente, preço, prazo)
+%entrega(nr, estafeta, cliente, meio_de_transporte, volume, date_de_entrega)
+
+g([encomenda(1, c1, 15, 18/11/2021), encomenda(2, c1, 25, 16/11/2021), encomenda(3, c2, 26, 18/11/2021)], [entrega(1, e1, c1, bicicleta, 4, 18/11/2021), entrega(2, e2, c1, moto, 6, 16/11/2021), entrega(3, e1, c2, moto, 7, 18/11/2021)]).
+
 
 %entrega_estafeta______________peso_volume_maybe_____________
 %classificação_nota
@@ -28,7 +30,7 @@ g([entrega(1, n1, c1, bicicleta, 4), entrega(2, n2, c1, moto, 6), entrega(2, n1,
 
 %2
 identificar_estafetas([], _, []).
-identificar_estafetas([entrega(_,E,C,_,_)|T], C, [E|S]):- 
+identificar_estafetas([entrega(_,E,C,_,_,_)|T], C, [E|S]):- 
 	estafeta(E), cliente(C),
 	identificar_estafetas(T, C, S), not(member(E,S)), !.
 
@@ -36,17 +38,23 @@ identificar_estafetas([_|T], C, S):- identificar_estafetas(T, C, S).
 
 
 %3
-
-clientes_servidos(E, S):- g(L), clientes_servidos(E, L, S).
+clientes_servidos(E, S):- g(_, L), clientes_servidos(E, L, S).
 
 clientes_servidos(_, [], []).
-clientes_servidos(E, [entrega(_,E,X,_,_)|T], [X|S]):- 
+clientes_servidos(E, [entrega(_,E,X,_,_,_)|T], [X|S]):- 
 	estafeta(E), cliente(X),
 	clientes_servidos(E, T, S),
-	not(member(X,S)).
+	not(member(X,S)), !.
 clientes_servidos(E, [_|T], S):- clientes_servidos(E, T, S).
 
 %4
+valor_faturado(D/M/A, S):- g(L, _), valor_faturado(D/M/A, L, S).
+
+valor_faturado(_, [], 0).
+valor_faturado(D/M/A, [encomenda(_,_,P,D/M/A)|T], S):-
+	valor_faturado(D/M/A, T, S1),
+	S is S1 + P, !.
+valor_faturado(D/M/A, [_|T], S):- valor_faturado(D/M/A, T, S).
 
 %5
 
@@ -54,10 +62,25 @@ clientes_servidos(E, [_|T], S):- clientes_servidos(E, T, S).
 
 %7
 %adicionar intervalo de tempo
+%!
+%!
+%!
+%!
+%!
+%!
+%!
+%!
+%!
+
+
+
+
+
+
 p_transportes(B, M, C):- g(L), p_transporte(bicicleta, L, B), p_transporte(moto, L, M), p_transporte(carro, L, C). 
 
 p_transporte(_, [], 0).
-p_transporte(V, [entrega(_,_,_,V,_)|T], S):- 
+p_transporte(V, [entrega(_,_,_,V,_,_)|T], S):- 
 	p_transporte(V, T, S1),
 	S is S1 + 1, !.
 p_transporte(V, [_|T], S):- p_transporte(V, T, S).
@@ -67,11 +90,10 @@ p_transporte(V, [_|T], S):- p_transporte(V, T, S).
 %9
 
 %10
-%adicionar dia
-peso_carregado(E, S):- g(L), peso_carregado(E, L, S).
+peso_carregado(E, D/M/A, S):- g(_, L), peso_carregado(E, D/M/A, L, S).
 
-peso_carregado(_, [], 0).
-peso_carregado(E, [entrega(_,E,_,_,N)|T], S):- 
-	peso_carregado(E,T,S1), S is S1 + N, !.
-peso_carregado(E, [_|T], S):- peso_carregado(E,T,S).
+peso_carregado(_, _, [], 0).
+peso_carregado(E, D/M/A, [entrega(_,E,_,_,N,D/M/A)|T], S):- 
+	peso_carregado(E,D/M/A,T,S1), S is S1 + N, !.
+peso_carregado(E, D/M/A, [_|T], S):- peso_carregado(E, D/M/A, T, S).
 
