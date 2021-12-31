862ac7d865b1aabe92adafbe33252ec2ca91cac9
RayMightBeWrong <ruizinho7braga@gmail.com>
2021-12-31 15:56:50 +0000

    DFS feito, let's go

---
+++ fase2/helper.pl
@@ -0,0 +1,38 @@
+props_transporte(bicicleta, 5, 10).
+props_transporte(moto, 20, 35).
+props_transporte(carro, 100, 25).
+
+%funções chamam-se isget, porque:
+%servem para verificar se o elemento é o mesmo (is) e
+%servem para ir buscar o elemento (get)
+
+isget_estafeta(E, entrega(E,_)).
+isget_veiculo(V, entrega(_,transporte(V,_))).
+isget_coef(C, entrega(_,transporte(_,C))).
+
+isget_cliente(C, encomenda(C,_,_,_,_)). 
+isget_local(L, encomenda(_,L,_,_,_)).
+isget_peso(P, encomenda(_,_,P,_,_)).
+isget_preco(P, encomenda(_,_,_,P,_)).
+isget_nota(N, encomenda(_,_,_,_,N)).
+
+isget_hora_limite(H/M, prazo(H/M,_,_,_)).
+isget_data_limite(D/M/A, prazo(_,D/M/A,_,_)).
+isget_hora_entrega(0, prazo(_,_, 0,_)).
+isget_hora_entrega(H/M, prazo(_,_, H/M ,_)).
+isget_data_entrega(0, prazo(_, _, _, 0)).
+isget_data_entrega(D/M/A, prazo(_, _, _, D/M/A)).
+
+
+entrega_in_time(Hi, Di, Hf, Df, prazo(_, _, HE, DE)):-
+	is_older_than(Hi, Di, HE, DE), is_older_than(HE, DE, Hf, Df).
+
+is_older_than(_, _/_/A1, _/_, _/_/A2):- A1 < A2, !.
+is_older_than(_, _/M1/A1, _, _/M2/A2):- 
+	A1 =:= A2, M1 < M2, !. 
+is_older_than(_, D1/M1/A1, _, D2/M2/A2):- 
+	A1 =:= A2, M1 =:= M2, D1 < D2, !. 
+is_older_than(H1/_, D1/M1/A1, H2/_, D2/M2/A2):- 
+	A1 =:= A2, M1 =:= M2, D1 =:= D2, H1 < H2, !. 
+is_older_than(H1/MIN1, D1/M1/A1, H2/MIN2, D2/M2/A2):- 
+	A1 =:= A2, M1 =:= M2, D1 =:= D2, H1 =:= H2, MIN1 =< MIN2, !. 
--- fase2/tp.pl
+++ fase2/tp.pl
@@ -1,12 +1,16 @@
 :- include('mapa.pl').
+:- include('helper.pl').
 
-gera_caminho(A, B, S):- gera_caminhoL(A, [B], S).
-
-gera_caminhoL(A, [A|T], [A|T]).
-gera_caminhoL(A, [B|T], S):- adjacente(X, B, _, _), not(member(X, [B|T])),
-				gera_caminhoL(A, [X,B|T], S).
+%
+%gera_todos(A, B, T, P, L):- findall(S, gera_circuito(A, B, T, P, S), L).
 
 
+% A e B -> nodos de partida e de chegada, respetivamente
+% T -> transporte
+% P -> peso da encomenda
+circuitoDFS(A, B, T, P, circuito(A, B, T, P, S)):- circuitoDFS(A, [B], [], S).
 
-gera_todos(A, B, L):- findall(S, gera_caminho(A, B, S), L).
+circuitoDFS(A, [A|_], S, S).
+circuitoDFS(A, [B|T], L, S):- adjacente(X, B, D, C), not(member(X, [B|T])),
+				circuitoDFS(A, [X,B|T], [aresta(X, B, D, C)|L], S).
 
