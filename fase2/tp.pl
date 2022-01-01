0e5221246778316d2eac28fc1f05e77e663a3fcb
RayMightBeWrong <ruizinho7braga@gmail.com>
2022-01-01 15:30:41 +0000

    BFS feito, -1

--- fase2/tp.pl
+++ fase2/tp.pl
@@ -8,9 +8,47 @@
 % A e B -> nodos de partida e de chegada, respetivamente
 % T -> transporte
 % P -> peso da encomenda
-circuitoDFS(A, B, T, P, circuito(A, B, T, P, S)):- circuitoDFS(A, [B], [], S).
 
-circuitoDFS(A, [A|_], S, S).
-circuitoDFS(A, [B|T], L, S):- adjacente(X, B, D, C), not(member(X, [B|T])),
-				circuitoDFS(A, [X,B|T], [aresta(X, B, D, C)|L], S).
+circuitoDFS(A, B, T, P, circuito(A, B, T, P, S)):- circuitoDFS(A, [B], S).
+
+circuitoDFS(A, [A|T], [A|T]).
+circuitoDFS(A, [B|T], S):- adjacente(X, B, _, _), not(member(X, [B|T])),
+				circuitoDFS(A, [X,B|T], S).
+			
+
+circuitoBFS(A, B, T, P, circuito(A, B, T, P, S)):- circuitoBFS(B, [[A]], S).
+
+circuitoBFS(F, [[F|T]|_], S):- reverse([F|T], S). %write("final: "), write(S), write("\n").
+
+circuitoBFS(B, [EstadosA|Outros], S):-
+	EstadosA = [Atual|_],
+	findall([X|EstadosA],
+		(B \== Atual, adjacente(Atual, X, _, _), not(member(X, EstadosA))), Novos),
+	append(Outros, Novos, Todos),
+	%write("Nodo atual: "), write(Atual), write("\n"),
+	%write("EstadosA: "), write(EstadosA), write("\n"),
+	%write("Novos: "), write(Novos), write("\n"),
+	%write("Outros: "), write(Outros), write("\n"),
+	%write("estado: "), write(Todos), write("\n"), write("\n"),
+	circuitoBFS(B, Todos, S).
+
+
+%getArestaFromNovos -> appendArestasToListOfLists
+createListAresta([_], []).
+createListAresta([A,B|T1], [aresta(A, B, D, C)|T2]):- adjacente(A, B, D, C), createListAresta([B|T1], T2).
+
+
+
+%solve(S):- bfs(jarros(0,0), jarros(_,4), S).
+
+%bfs(EstadoI, EstadoF, S):- bfs_M(EstadoF, [[EstadoI]], S).
+
+%bfs_M(EstadoF, [[EstadoF|T]|_], S):- reverse([EstadoF|T], S).
+
+%bfs_M(EstadoF, [EstadosA|Outros], S):-
+%	EstadosA = [Act|_],
+%	findall([EstadoX|EstadosA],
+%		(EstadoF \== Act, transicao(Act, Move, EstadoX), \+member(EstadoX, EstadosA)), Novos),
+%	append(Outros, Novos, Todos),
+%	bfs_M(EstadoF, Todos, S).
 
